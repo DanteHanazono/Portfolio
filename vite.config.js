c9a100f4c708a903d241ab12b7ccf8b7f689476d
Rhady <rslopez0611@gmail.com>
2024-02-28 15:52:18 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/style.css',
                 'resources/js/app.js',
+                'resources/js/script.js',
             ],
             refresh: true,
         }),
